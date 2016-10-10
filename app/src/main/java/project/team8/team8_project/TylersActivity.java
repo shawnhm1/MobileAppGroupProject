86f1c859df85f954802e6fe9e8777f53b1a39ba2
tylerkb2 <get2tylerb@gmail.com>
2016-10-10 16:30:30 -0700

    Added TylersActivity

---
+++ app/src/main/java/project/team8/team8_project/TylersActivity.java
@@ -0,0 +1,13 @@
+package project.team8.team8_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TylersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tylers);
+    }
+}
