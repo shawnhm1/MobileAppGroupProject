0c5c9f67f4bd802f7a3d78ff7536b0e4b12ab347
Shawn Massoud <shawnhm1@gmail.com>
2016-10-10 16:53:08 -0700

    Shawn Activity

---
+++ app/src/main/java/project/team8/team8_project/shawnActivity.java
@@ -0,0 +1,16 @@
+package project.team8.team8_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shawn on 10/10/2016.
+ */
+
+public class shawnActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tylers);
+    }
+}
